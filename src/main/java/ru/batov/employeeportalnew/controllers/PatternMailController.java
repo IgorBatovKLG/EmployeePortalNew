ce7f5de551b197c6179a55f534c95268538e2aa8
work <igorbatovg@gmail.com>
2024-02-27 22:18:14 +0300

    initial commit

--- src/main/java/ru/batov/employeeportalnew/controllers/PatternMailController.java
+++ src/main/java/ru/batov/employeeportalnew/controllers/PatternMailController.java
@@ -1,10 +1,12 @@
 package ru.batov.employeeportalnew.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PatternMailController {
 
+    @GetMapping("/creatingMailForm")
     public String creatingMailForm(){
         //todo creatingMailForm
         return "mail/creating_form.html";
